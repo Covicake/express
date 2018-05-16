aa8658df2bf52af605dd5531d8592f60fd616104
Fernando <fernand-o.a@hotmail.com>
2018-05-16 20:32:21 +0100

    moved to controller and handled errors

--- src/api/movies/controller.js
+++ src/api/movies/controller.js
@@ -6,6 +6,11 @@ const forbiddenKeys = ["id", "like"];
 files.loadData(moviesData => movies = moviesData);
 
 
+function getLikes(){
+  let likedMovies = movies.filter(movie => movie.like == true);
+  return likedMovies;
+}
+
 function getMovies(){
   return movies;
 }
@@ -19,19 +24,91 @@ function getMovie(movieId){
   }
 }
 
-function modifyMovie(){
+function postMovie(body, callback){
+  if(Object.keys(body).length > 0){
+    console.log(body);
+    const newMovie = body;
 
+    const movie = movies.find(movie => movie.title.toLowerCase() === newMovie.title.toLowerCase());
+    console.log(movie);
+    if(movie == undefined){
+      if(movies.length>0){
+        newMovie.id = String(Number(movies[movies.length-1].id)+1);
+      }else{
+        newMovie.id = "0";
+      }
+      if(newMovie.like == undefined){
+        newMovie.like = false;
+      }
+      movies.push(newMovie);
+
+      files.saveData(movies, save_response => {
+        if(save_response){
+          callback("save err");
+        }else{
+          callback(newMovie);
+        }
+      });
+    }else{
+      callback("Pelicula repetida");
+    }
+  }else {
+    callback(400);
+  }
 }
 
-function deleteMovie(){
-
+function modifyMovie(movieId, newMovie, callback){
+  const movieIndex = movies.findIndex(movie => movie.id === movieId);
+  newMovie.id = movies[movieIndex].id;
+  const movies_keys = Object.keys(movies[movieIndex]);
+  Object.keys(newMovie).forEach(key => {
+    if(!movies_keys.includes(key) || forbiddenKeys.includes(key)){
+      delete newMovie[key];
+    }
+  });
+  const movie = {...movies[movieIndex], ...newMovie}
+  movies[movieIndex] = movie;
+  files.saveData(movies, save_response => {
+    if(save_response){
+      callback("save err");
+    }else{
+      callback(movies[movieIndex]);
+    }
+  });
 }
 
-function getLikes(){
-  let likedMovies = movies.filter(movie => movie.like == true);
-  return likedMovies;
+function deleteMovie(movieId, callback){
+  const movieIndex = movies.findIndex(movie => movie.id === movieId);
+  if(movieIndex >= 0){
+    movies.splice(movieIndex, 1);
+    files.saveData(movies, save_response => {
+      if(save_response){
+        callback("save err");
+      }else{
+        callback(200);
+      }
+    });
+  }else{
+    callback(400);
+  }
 }
 
-function toggleLike(){
 
+
+function toggleLike(movieId, callback){
+  const movieIndex = movies.findIndex(movie => movie.id === movieId);
+  if(movieIndex >= 0){
+    if(movies[movieIndex].like == false){
+      movies[movieIndex].like = true;
+    }else{
+      movies[movieIndex].like = false;
+    }
+    files.saveData(movies, save_response => {
+      if(save_response){
+        callback("save err")
+      }else{
+        callback(movies[movieIndex]);
+      }
+    });
+  }
 }
--- src/api/movies/index.js
+++ src/api/movies/index.js
@@ -8,97 +8,45 @@ router.get('/likes', (req, res) => res.send(controller.getLikes()));
 
 router.get('/', (req, res) => res.send(controller.getMovies()));
 
-router.get('/:id', (req, res) =>res.send(controller.getMovie(req.params.id)));
+router.get('/:id', (req, res) => res.send(controller.getMovie(req.params.id)));
 
-router.post('/', (req, res) =>{
-  if(Object.keys(req.body).length > 0){
-    console.log(req.body);
-    const newMovie = req.body;
-
-    const movie = movies.find(movie => movie.title.toLowerCase() === newMovie.title.toLowerCase());
-    console.log(movie);
-    if(movie == undefined){
-      if(movies.length>0){
-        newMovie.id = String(Number(movies[movies.length-1].id)+1);
-      }else{
-        newMovie.id = "0";
-      }
-      if(newMovie.like == undefined){
-        newMovie.like = false;
-      }
-      movies.push(newMovie);
-
-      files.saveData(movies, save_response => {
-        if(save_response){
-          res.error(save_response);
-        }else{
-          res.json(newMovie);
-        }
-      });
-    }else{
-      res.send("Pelicula repetida");
-    }
-  }else {
-    res.status(400).send("Debes enviar algo en el body");
-  }
-});
-
-router.put('/:id', (req, res) =>{
-  const movieId = req.params.id;
-  let newMovie = req.body;
-  const movieIndex = movies.findIndex(movie => movie.id === movieId);
-  newMovie.id = movies[movieIndex].id;
-  const movies_keys = Object.keys(movies[movieIndex]);
-  Object.keys(newMovie).forEach(key => {
-    if(!movies_keys.includes(key) || forbiddenKeys.includes(key)){
-      delete newMovie[key];
-    }
-  });
-  const movie = {...movies[movieIndex], ...newMovie}
-  movies[movieIndex] = movie;
-  files.saveData(movies, save_response => {
-    if(save_response){
-      res.error(save_response);
-    }else{
-      res.json(movies[movieIndex]);
-    }
-  });
-});
-
-router.delete('/:id', (req, res) =>{
-  const movieId = req.params.id;
-  const movieIndex = movies.findIndex(movie => movie.id === movieId);
-  if(movieIndex >= 0){
-    movies.splice(movieIndex, 1);
-    files.saveData(movies, save_response => {
-      if(save_response){
-        res.error(save_response);
-      }else{
-        res.send(200)
-      }
-    });
+router.post('/', (req, res) => { controller.postMovie(req.body, response =>{
+  if(response == 400){
+    res.status(400).send("Es necesario enviar algo en el body");
+  }else if(response == "save err"){
+    res.error("Error while saving");
   }else{
-    res.status(400).send("Debes enviar un id valido");
+    res.send(response);
   }
-
+  });
 });
 
-router.put('/:id/toggle_like', (req, res) =>{
-  const movieId = req.params.id;
-  const movieIndex = movies.findIndex(movie => movie.id === movieId);
-  if(movieIndex >= 0){
-    if(movies[movieIndex].like == false){
-      movies[movieIndex].like = true;
-    }else{
-      movies[movieIndex].like = false;
-    }
-    files.saveData(movies, save_response => {
-      if(save_response){
-        res.error(save_response);
-      }else{
-        res.json(movies[movieIndex]);
-      }
-    });
+router.put('/:id', (req, res) =>{ controller.modifyMovie(req.params.id, req.body, response => {
+  if(response == "save err"){
+    res.error("Error while saving");
+  }else{
+    res.send(response);
   }
+  });
+});
+
+router.delete('/:id', (req, res) =>{ controller.deleteMovie(req.params.id, response => {
+  if(response == "save err"){
+    res.send("Error while saving");
+  }else if(response == 400){
+    res.status(400).send("Debes enviar un id valido");
+  }else{
+    res.send(response);
+  }
+  });
+});
+
+router.put('/:id/toggle_like', (req, res) =>{ controller.toggleLike(req.params.id, response =>{
+  if(response == "save err"){
+    res.error("Error while saving");
+  }else{
+    res.send(response);
+  }
+  });
 });
 module.exports = router;
