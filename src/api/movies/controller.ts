173f539f372d876c5517c1ab68be03893d8c4006
Fernando <fernand-o.a@hotmail.com>
2018-05-23 20:10:40 +0100

    Migrating to mongo!

--- src/api/movies/controller.ts
+++ src/api/movies/controller.ts
@@ -1,9 +1,11 @@
 import { loadData, saveData } from '../../utils/files';
 import { Movie } from '../../classes/Movie';
+import { MongoClient, Server, ObjectId } from 'mongodb';
 
 let movies: Movie[];
 const forbiddenKeys = ['id', 'like'];
 const movieKeys = ['id', 'title', 'like'];
+const MONGOURL = 'mongodb://localhost:27017';
 
 // Deletes any element not present on "movieKeys" and present on "forbiddenKeys" from the request body.
 function cleanReq(requestMovie: Movie): Movie {
@@ -28,51 +30,46 @@ export function getMovies(): Movie[] {
 
 export function getMovie(movieId: string): Promise<any> {
   return new Promise((resolve, reject) => {
-    const movieResult = movies.find((movie) => movie.id === movieId);
-    if (movieResult !== undefined) {
-      resolve(movieResult);
-    } else {
-      reject(400);
-    }
+    MongoClient.connect(MONGOURL, (err, client) => {
+      if (!err) {
+        const db = client.db('moviesDB');
+        const moviesCollection = db.collection('movies');
+        moviesCollection.findOne({ _id: new ObjectId(movieId) })
+        .then((movie) => resolve(movie))
+        .catch((error) => reject(error));
+      } else {
+        reject(err);
+      }
+    });
   });
 }
 
-export function postMovie(body) {
+export function postMovie(body): Promise<any> {
   return new Promise((resolve, reject) => {
-    if (Object.keys(body).length > 0) {
-      const newMovie = cleanReq(body);
-      const movieResult = movies.find((movie) => movie.title.toLowerCase() === newMovie.title.toLowerCase());
-
-      if (movieResult === undefined) {
-        newMovie.id = movies.length > 0 ? '' + (Number(movies[movies.length - 1].id) + 1) : '0';
-        newMovie.like = false;
-        movies.push(newMovie);
-
-        saveData(movies, (saveResponse) => {
-          if (saveResponse) {
-            reject('save err');
-          } else {
-            resolve(newMovie);
-          }
-        });
+    const movieToInsert = { ...body, created: new Date(), updated: new Date()};
+    MongoClient.connect(MONGOURL, (err, client) => {
+      if (!err) {
+        const db = client.db('moviesDB');
+        const moviesCollection = db.collection('movies');
+        moviesCollection.insertOne(movieToInsert)
+          .then(() => resolve())
+          .catch((error) => reject(error));
       } else {
-        reject('Pelicula repetida');
+        reject(err);
       }
-    } else {
-      reject(400);
-    }
+    });
   });
 }
 
 export function modifyMovie(movieId: string, newMovie: Movie): Promise<any> {
   return new Promise((resolve, reject) => {
     const movieIndex = movies.findIndex((movie) => movie.id === movieId);
+
     if (!movieIndex) {
       reject(400); // Invalid Id
     } else {
       newMovie = cleanReq(newMovie);
       const movie: Movie = {...movies[movieIndex], ...newMovie};
-
       movies[movieIndex] = movie;
 
       saveData(movies, (saveResponse) => {
@@ -89,8 +86,10 @@ export function modifyMovie(movieId: string, newMovie: Movie): Promise<any> {
 export function deleteMovie(movieId: string): Promise<any> {
   return new Promise((resolve, reject) => {
     const movieIndex = movies.findIndex((movie) => movie.id === movieId);
+
     if (movieIndex >= 0) {
       movies.splice(movieIndex, 1);
+
       saveData(movies, (saveResponse) => {
         if (saveResponse) {
           reject('save err');
@@ -98,6 +97,7 @@ export function deleteMovie(movieId: string): Promise<any> {
           resolve(200);
         }
       });
+
     } else {
       reject(400);
     }
@@ -107,8 +107,10 @@ export function deleteMovie(movieId: string): Promise<any> {
 export function toggleLike(movieId: string): Promise<any> {
   return new Promise((resolve, reject) => {
     const movieIndex = movies.findIndex((movie) => movie.id === movieId);
+
     if (movieIndex >= 0) {
       movies[movieIndex].like = movies[movieIndex].like === false ? true : false;
+
       saveData(movies, (saveResponse) => {
         if (saveResponse) {
           reject('save err');
